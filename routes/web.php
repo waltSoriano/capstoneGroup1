f414af755c599e22b74d824f99592f8fc16a5857
waltSoriano <135300851+waltSoriano@users.noreply.github.com>
2023-08-12 00:00:44 +0800

    changes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/about', function () {
+    return '<h2>i am about page</h2>';
+});
